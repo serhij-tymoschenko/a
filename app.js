bab7bc78489108a03d20d188d21e6c359aa624ba
Serhij <serhij.tymoschenko@gmail.com>
2025-10-04 18:07:52 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,1 @@
-if (window.location.href.includes("#")) {
-    const startIndex = window.location.href.indexOf('#') + 2;
-    window.location.replace(`reddit://avatar/shop/product/storefront_nft_01K50HWH2YR4P53ZGXK8MD19M1`)
-}
+window.location.replace(`reddit://avatar/shop/product/storefront_nft_01K50HWH2YR4P53ZGXK8MD19M1`)
