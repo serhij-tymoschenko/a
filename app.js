e6e9e2d88b3d49adad80ebbf6c78da4935c45d21
Serhij <serhij.tymoschenko@gmail.com>
2025-10-04 18:11:49 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,1 @@
-window.location.replace(`reddit://avatar/shop/product/storefront_nft_01K50HWH2YR4P53ZGXK8MD19M1`)
+window.location.replace(`reddit://reddit/avatar/shop/product/storefront_nft_01K50HWH2YR4P53ZGXK8MD19M1`)
